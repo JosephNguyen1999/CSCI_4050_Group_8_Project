9bbc09e7a5d7b451e39f962acbf0532d12627699
Tyler Calhoun <tylercalhoun@gmail.com>
2022-04-20 13:23:24 -0400

    changes to registration

--- registration_page.php
+++ registration_page.php
@@ -36,6 +36,30 @@
 		}
 	</style>
 
+	<script src=
+    	"https://smtpjs.com/v3/smtp.js">
+  	</script>
+  
+  	<script type="text/javascript">
+    	function sendEmail() {
+      		//need to generate code
+			//put the code in the database
+			var val = document.getElementById("email").value;
+			Email.send({
+        	Host: "smtp.gmail.com",
+        	Username: "sender@email_address.com",
+        	Password: "Enter your password",
+        	To: val,
+        	From: "sender@email_address.com",
+        	Subject: "Sending Email using javascript",
+        	Body: "Well that was easy!!",
+      	})
+        .then(function (message) {
+          alert("mail sent successfully")
+        });
+    }
+  	</script>
+
 </head>
 
 
